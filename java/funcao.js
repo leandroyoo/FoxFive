c2c9f7f735cbb4a3174246ce9bf2173c66ae6ae7
leandroyoo <94478634+leandroyoo@users.noreply.github.com>
2022-12-14 13:07:44 -0300

    Add files via upload

---
+++ java/funcao.js
@@ -0,0 +1,7 @@
+function formu(){
+    f = (document.form.num1) + (document.form.num2) + (document.form.num3) + (document.form.num4)+ (document.form.num5)
+    alert("CADASTRO REALIZADO COM SUCESSO!")}
+
+function msg(){
+        f = (document.form.num1) + (document.form.num2) + (document.form.num3)
+        alert("SUA MENSSAGEM FOI ENVIADA COM SUCESSO!")}
